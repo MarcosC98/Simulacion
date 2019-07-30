14b582bd0406c6aae6883198dfee60790937aa55
rpenalozav <47232474+rpenalozav@users.noreply.github.com>
2019-07-29 22:07:24 -0500

    Grafo y camino mas corto

---
+++ Simulacion/src/ejecucion/GrafoVia.scala
@@ -0,0 +1,16 @@
+package ejecucion
+import mapa.{Interseccion, Via}
+import scalax.collection.mutable.Graph
+import scalax.collection.edge.{WLDiEdge, WLUnDiEdge}
+
+import scala.collection.mutable.ArrayBuffer
+
+object GrafoVia {
+  val grafo =Graph[Interseccion,WLUnDiEdge]()
+  def construir(vias:ArrayBuffer[Via]): Unit ={
+    vias.foreach(r => grafo.add(WLUnDiEdge.apply(r.origen,r.fin)(r.distancia,r.nombreVia)))
+  }
+  def Dijkstra(origen: Interseccion, destino: Interseccion): Option[GrafoVia.grafo.Path] = {
+    grafo.get(origen).shortestPathTo(grafo.get(destino))
+  }
+}
--- Simulacion/src/mapa/Via.scala
+++ Simulacion/src/mapa/Via.scala
@@ -1,8 +1,10 @@
 package mapa
 import plano.Recta
-class Via (iorigen:Interseccion,ifinal:Interseccion,velmax:Int,
+class Via (iorigen:Interseccion,ifinal:Interseccion,velmax:Int,var
     nombreVia:String,tipoVia:TipoVia,sentido:Sentido)extends Recta {
   type T = Interseccion
   val origen = iorigen
   val fin = ifinal
+  def distancia=Math.sqrt(Math.pow(fin.x - origen.x,2) + Math.pow(fin.y - origen.y,2))
+
 }
