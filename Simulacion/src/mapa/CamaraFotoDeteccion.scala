56ad404383322a80aadec7e85a0024fdbbe65565
MarcosC98 <47372077+MarcosC98@users.noreply.github.com>
2019-08-25 23:59:21 -0500

    Add files via upload

---
+++ Simulacion/src/mapa/CamaraFotoDeteccion.scala
@@ -0,0 +1,28 @@
+package mapa
+import ejecucion.Simulacion
+import plano.Punto
+
+class CamaraFotoDeteccion(_via :Via,_distanciaDeOrigen:Int) {
+  
+  def via = _via
+  def distanciaDeOrigen = _distanciaDeOrigen
+  Simulacion.listaCamaras.append(this)
+ val angulo = Simulacion.calcularTanInv(via.origen.x, via.fin.x, via.origen.y, via.fin.y)
+ var posicion:Punto = null
+  if(angulo == 90){
+    posicion = Punto(via.origen.x,via.origen.y + distanciaDeOrigen)
+  }
+  else if(angulo ==180){
+    posicion = Punto(via.origen.x - distanciaDeOrigen, via.origen.y)
+  }
+  else if(angulo == 270){
+    posicion = Punto(via.origen.x,via.origen.y - distanciaDeOrigen)
+  }
+  else if(angulo == 0){
+    posicion = Punto(via.origen.x + distanciaDeOrigen,via.origen.y)
+  }else{
+    posicion = Punto( via.origen.x + math.cos(angulo.toRadians)*distanciaDeOrigen,via.origen.y+math.sin(angulo.toRadians)*distanciaDeOrigen)
+  }
+  via.tieneCamara = true
+  via.camara = this
+}
---
+++ Simulacion/src/mapa/Comparendo.scala
@@ -0,0 +1,7 @@
+package mapa
+import movimiento.Velocidad
+import vehiculos.Vehiculo
+import ejecucion.Simulacion
+class Comparendo (vehiculo:Vehiculo,velocidad:Double,velocidadMaxima:Double){
+  Simulacion.listaComparendos.append(this)
+}
